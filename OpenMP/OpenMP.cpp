a3030dd5e27d1f5478d63c62e194a1327a5b0fd1
astroboi-v <acortes.ie@gmail.com>
2025-01-25 23:49:30 -0600

    Comentarios adicionales al codigo inicial

--- OpenMP/OpenMP.cpp
+++ OpenMP/OpenMP.cpp
@@ -2,9 +2,9 @@
 //
 
 #include <iostream>
-
 #include <omp.h>
 
+// Globales
 #define N 1000
 #define chunk 100
 #define mostrar 10
@@ -13,9 +13,12 @@ void imprimeArreglo(float* d);
 
 int main() {
     std::cout << "Sumando arreglos en paralelo!\n" << std::endl;
+    
+    // Declaración de variables
     float a[N], b[N], c[N];
     int i;
 
+    // Inicialización de los arreglos
     for (i = 0; i < N; i++) {
         a[i] = i * 10;
         b[i] = (i + 3) * 3.7;
@@ -23,11 +26,14 @@ int main() {
 
     int pedazos = chunk;
 
+    // Definición de la estrategia de paralelización con OpenMP
     #pragma omp parallel for shared(a, b, c, pedazos) private(i) schedule(static, pedazos)
     
+    // Bucle paralelizado
     for (i = 0; i < N; i++)
         c[i] = a[i] + b[i];
 
+    // Imprime en pantalla los arreglos a, b y c.
     std::cout << "Imprimiendo lso primeros " << mostrar << " valores del arreglo a: " << std::endl;
     imprimeArreglo(a);
     std::cout << "Imprimiendo lso primeros " << mostrar << " valores del arreglo b: " << std::endl;
@@ -38,6 +44,7 @@ int main() {
     return 0;
 }
 
+// Imprime elementos del arreglo apuntado hasta el número definido en 'mostrar' 
 void imprimeArreglo(float* d) {
     for (int x = 0; x < mostrar; x++)
         std::cout << d[x] << " - ";
